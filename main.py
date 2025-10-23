5ed8fe9ef4f0087a69baa781a72f4e6d15367d53
ClaudioK5 <klaudio.asaro5@gmail.com>
2025-10-23 15:58:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import subprocess
 from generate_report import generate_financial_report
 
-balance_sheet_process = subprocess.run(['python', 'Balance sheet/main.py'], capture_output=True, text=True)
+balance_sheet_process = subprocess.run(['python', 'Balance sheet Equity/main.py'], capture_output=True, text=True)
 
 final_sum = float(balance_sheet_process.stdout.strip())
+
+balance_sheet_process_liabilities = subprocess.run(['python', 'Balance sheet Liabilities/main.py'], capture_output=True, text=True)
+
+final_sum_liabilities = float(balance_sheet_process_liabilities.stdout.strip())
+
+company_equity = final_sum - final_sum_liabilities
+
+print(final_sum_liabilities)
 
 net_profits_process = subprocess.run(['python', 'Net Profits/main.py'], capture_output=True, text=True)
 
@@ -13,5 +21,5 @@ total_cost = float(net_profits_process.stdout.strip().splitlines()[1])
 
 total_revenue = float(net_profits_process.stdout.strip().splitlines()[2])
 
-generate_financial_report(net_profits, total_revenue, total_cost, final_sum)
+generate_financial_report(net_profits, total_revenue, total_cost, final_sum, final_sum_liabilities, company_equity)
 
